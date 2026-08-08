0947d62dacbc113d58010a07ea113003538defb8
LH-10 <lalit1652h@gmail.com>
2026-08-08 16:50:33 +0530

    license in src code

--- Service/Signaling/CMD/main.go
+++ Service/Signaling/CMD/main.go
@@ -1,3 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0
+
+//TermBuzz
+// Copyright (C) 2026  Lalit Hinduja
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation;
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
 package main
 
 import (
